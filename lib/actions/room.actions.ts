4efeafb7a4b96c72003f7b8f4ff1249dc094ced9
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-22 12:06:29 +0530

    document listing is completed

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -64,3 +64,14 @@ export const updateDocument = async (roomId: string, title: string ) => {
     console.log("Error updating document", error);    
   }
 };
+
+
+export const getDocuments = async ( email: string ) => {
+  try {
+    const rooms = await liveblocks.getRooms({userId: email});
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log("Error getting documents", error);
+  }
+};
